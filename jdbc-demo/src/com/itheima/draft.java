e289f562af12c8b686779d7a26c08ce1dda4dfcf
Evan <3100591749@qq.com>
2022-10-28 15:10:07 +0800

    改了draft

--- jdbc-demo/src/com/itheima/draft.java
+++ jdbc-demo/src/com/itheima/draft.java
@@ -1,12 +1,22 @@
 package com.itheima;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class draft {
     public static void main(String[] args) {
-        int [] a = {1,2,3,4,5};
-        System.out.println(Arrays.toString(a));
+        String pattern = "([0-9]+),([0-9]+)";
+//        String pattern = "(^[0-9]*$),(^[0-9]*$)";
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String str = scanner.nextLine();
+            if (Pattern.matches(pattern, str)) {
+                System.out.println("correct input");
+            } else if (Objects.equals(str, "exit")) {
+                System.exit(0);
+            } else {
+                System.out.println("incorrect input");
+            }
+        }
     }
 }
